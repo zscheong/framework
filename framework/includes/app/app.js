eb798b749d6a4e4ef215bd3dbec190c28ad84500
zscheong <zscheong@gmail.com>
2015-06-09 17:49:50 +0800

    A Math Service added

--- framework/includes/app/app.js
+++ framework/includes/app/app.js
@@ -1,7 +1,26 @@
-var app = angular.module('AppModule',['ngAnimate']);
+var app = angular.module('AppModule',[]);
 
-app.factory('appConst', function() {
-   var app_const = {};
+app.factory('ConstService', function() {
+    var const_service = {
+        app_ver: '1.0',
+        app_name: 'client app'
+    };
    
-   return app_const;
+   return const_service;
+});
+
+app.factory('MathService', function() {
+    var methods = {
+        interpolate: function(x, x0, x1, y0, y1) {
+            if(isNaN(x0) || isNaN(x1) || 
+                isNaN(x) || isNaN(y0) || isNaN(y1)) {
+                return 0;
+            }            
+            if(x1 - x0 === 0) { return 0; }
+            var ratio = (y1-y0)/(x1-x0);
+            return x * ratio + y0;
+        }
+    };
+    
+    return methods;
 });
--- karma/test/angular/ServiceTest.js
+++ karma/test/angular/ServiceTest.js
@@ -12,4 +12,20 @@ describe('ConstServiceTest', function() {
     });
 });
 
+describe('MathService', function() {
+    beforeEach(module('AppModule'));
+    
+    var service;
+    
+    beforeEach(inject(function(MathService) {
+        service = MathService;
+    }));
+    
+    it("Interpolate Function - normal operation", function() {
+        var x0 = 10, x1 = 100, y0 = 12, y1 = 18;
+        var x = 60, result = 16;
+        
+        expect(service.interpolate(x, x0, x1, y0, y1)).toEqual(result); 
+    });
+});
     
